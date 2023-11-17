2e6b86747051efca1e5ff60a512b3ba743ea2fbc
Jason Atkins <jason_atkins@hotmail.co.uk>
2023-11-17 10:24:38 +0000

    Added test file to test webhook

--- lbg.py
+++ lbg.py
@@ -16,7 +16,7 @@ import mimetypes
 mimetypes.add_type('text/javascript', '.js')
 
 # set up the app with listening socket for http requests and appropriate hostname
-#PORT = 8080 
+#PORT = 8080  
 #HOST = '0.0.0.0'
 
 # get app to serve static files from the public directory
